80c9ad908ba59127ac3828836059d50c616bc163
u0652804 <u0652804@nkfust.edu.tw>
2018-12-05 21:22:23 +0800

    up
    up

---
+++ src/StarDilateMethod2/src/star/Star.java
@@ -0,0 +1,81 @@
+package star;
+
+/***
+ * @author Bo-Xun Liao
+ * @tech -- dilate
+ * 
+ ***/
+
+public class Star {
+	
+	public void printStars(int layerNum) {
+		
+		char printData[] = new char[layerNum * 2 - 1];
+		
+		//init. printdata
+		for(int i = 0; i < printData.length; i ++){
+			if(i == layerNum - 1)
+				printData[i] = '*';
+			else
+				printData[i] = ' ';
+		}
+		
+		//print first row
+		for(int j = 0; j < printData.length; j ++)
+			System.out.print(printData[j]);
+		System.out.println();
+		
+		char[] kernal1 = {' ', '*', ' '};
+		char[] kernal2 = {'*', ' ', '*'};
+		
+		
+		// use kernal1 trans data to kernal2
+		for(int i = 0; i < layerNum - 1; i ++)
+		{
+			//dilate
+			printData = dilater(kernal1, kernal2, printData);
+			
+			//print row
+			for(int j = 0; j < printData.length; j ++)
+				System.out.print(printData[j]);		
+			System.out.println();
+		}
+		
+	}
+	
+	public char[] dilater(char[] kernal, char[] transData ,char[] original)
+	{
+		char[] out = new char[original.length];
+		int[] neighbors = new int[kernal.length];
+		
+		//init. (copy data)
+		for(int i = 0; i < original.length; i ++)
+			out[i] = original[i];
+		
+		//init. neighbors
+		for(int i = 0; i < kernal.length; i ++)
+			neighbors[i] = i - kernal.length / 2;
+
+		//dilate
+		int kernalEdge = kernal.length / 2;
+		for(int i = kernalEdge; i < original.length - kernalEdge; i ++){
+			
+			//judge equal or not with kernal
+			boolean flag = true;
+			for(int j = 0; j < neighbors.length; j ++){
+				if(original[i + neighbors[j]] != kernal[j])
+					flag = false;
+			}
+			
+			//trans data to out[]
+			if(flag)
+			{
+				for(int j = 0; j < neighbors.length; j ++){
+					out[i + neighbors[j]] = transData[j];
+				}
+			}
+		}
+		
+		return out;
+	}
+}
---
+++ src/StarDilateMethod2/src/star/main.java
@@ -0,0 +1,26 @@
+package star;
+import java.util.Random;
+
+/***
+ * @author Bo-Xun Liao
+ * @tech -- dilate
+ * 
+ * print stars like below :
+ *      *
+ *     * *
+ *    * * *
+ *   * * * *
+ *  * * * * *
+ ***/
+
+//祐安資訊 面試題目作答 -- 
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Star star = new Star();
+		star.printStars(5);
+	}
+
+}
